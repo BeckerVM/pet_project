faa719c002de23a59a7f61f1c4401986a9b4a20d
unknown <beiker2203@gmail.com>
2020-08-22 19:44:00 -0500

    Realizando donaciones

---
+++ controllers/donation_controller.js
@@ -0,0 +1,53 @@
+const connection = require('../db/connection')
+const cloudinary = require('cloudinary').v2
+const fs = require('fs-extra')
+
+cloudinary.config({ 
+  cloud_name: 'dripm8g4s', 
+  api_key: '537988747936494', 
+  api_secret: 'eQn7ynR6lGimxTxBqU9qVB16FtM' 
+})
+
+const sendDonation = async (req, res) => {
+  const { nombre, direccion, celular, email, dni } = req.body
+  const resultc = await cloudinary.uploader.upload(req.file.path)
+
+  connection.query('SELECT * FROM donantes WHERE dni = ?', [dni], (err, donantes) =>  {
+    if(err) throw err;
+
+    if(donantes.length === 0) {
+      connection.query(
+        'INSERT INTO donantes (dni, nombrecompleto, direccion, celular, email) VALUES (?, ?, ?, ?, ?)',
+        [dni, nombre, direccion, celular, email],
+        (err, result) => {
+          if (err) throw err;
+
+          connection.query(
+            'INSERT INTO donaciones(dnidonante, fecha, url) VALUES (?, ?, ?)',
+            [dni, new Date().toLocaleDateString("en-US"), resultc.url],
+            async (err, result) => {
+              if (err) throw err;
+              await fs.unlink(req.file.path)
+              res.redirect('/')
+            }
+          )
+        }
+      )
+    } else {
+      connection.query(
+        'INSERT INTO donaciones(dnidonante, fecha, url) VALUES (?, ?, ?)',
+        [dni, new Date().toLocaleDateString("en-US"), resultc.url],
+        async (err, result) => {
+          if (err) throw err;
+          await fs.unlink(req.file.path)
+          res.redirect('/')
+        }
+      )
+    }
+    
+  })
+}
+
+module.exports = {
+  sendDonation
+}
--- routes/web.js
+++ routes/web.js
@@ -6,6 +6,7 @@ const { noAuthenticated } = require('../helpers/auth')
 //CONTROLADORES
 const indexController = require('../controllers/index_controller')
 const petController = require('../controllers/pet_controller')
+const donationController = require('../controllers/donation_controller')
 
 router.get('/',  noAuthenticated, indexController.getDataHome)
 
@@ -21,6 +22,8 @@ router.post('/pets/:petAge', petController.getPetsByAge)
 router.get('/pet/:petId',  noAuthenticated, petController.getPet)
 
 router.post('/pet/:petId', petController.getDataPetById)
+
+router.post('/donation', donationController.sendDonation)
 
 //AMANIDADES
 router.get('/history/:historyId',  noAuthenticated, indexController.getHistory)
